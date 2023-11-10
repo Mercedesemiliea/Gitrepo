a01469e6745611180fc4733225c32786068ea5b7
Elsa Eriksson <elsa.95.eriksson@gmail.com>
2023-11-10 15:12:41 +0100

    header

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,10 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:5173/')
-  })
-})
+
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+  });
+
+  it("should have a button", () => {
+cy.get("button").contains("hämta").should("exist")
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 import './style.css'
+
+
+
+function createHeader (){
+  const containerHeader = document.getElementById("header");
+const titleHeader = document.createElement("h1");
+titleHeader.innerHTML = "Testing";
+
+
+const buttonHeader = document.createElement("button");
+buttonHeader.innerHTML = "hämta titel";
+
+buttonHeader.addEventListener("click", () => {
+  const containerHeader = document.getElementById("header");
+const titleHeader = document.createElement("h1");
+titleHeader.innerHTML = "Testing";
+containerHeader?.appendChild(titleHeader);
+
+})
+
+containerHeader?.appendChild(buttonHeader);
+}
+ createHeader();
